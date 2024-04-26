aa47127d9066918c048661fc91fc0266fead6f80
icedjello <revues.counter0x@icloud.com>
2024-04-26 20:03:36 +0100

    add params & body

--- index.js
+++ index.js
@@ -1,14 +1,38 @@
 import express from "express";
 // const express = require("express");
-import "dotenv/config";
+// import "dotenv/config";
+
 // require("dotenv").config();
+// dotenv would be required before node v20
+// .env files are now supported.
 
 const portNumber = process.env.PORT_NUMBER;
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello, yes, what do you want? eh?");
+app.use(express.json());
+// Basically telling express to turn
+
+app.get("/", function (_, res) {
+  res.send("Hello! Yes, we're doing well!");
+});
+
+app.get("/read-body", function (req, res) {
+  // Example of how to read the body of a request.
+  // http://localhost:8000/read-body with a body of:
+  // { "good": "thing" } returns the same thing.
+  const { body } = req;
+  console.log("body:", body);
+  res.send(body);
+});
+
+app.get("/read-params/:val", (req, res) => {
+  // Example of how to read param.
+  // http://localhost:8000/read-params/hello
+  // Would return { "val": "hello" }
+  const { params } = req;
+  console.log("yep", params);
+  res.send(params);
 });
 
 app.listen(portNumber, () => console.log(`listening on port ${portNumber}`));
