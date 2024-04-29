37849c59f63ebf019714ced8ef35d5f44a395809
icedjello <revues.counter0x@icloud.com>
2024-04-29 18:50:58 +0100

    add comments

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const portNumber = process.env.PORT_NUMBER;
 const app = express();
 
 app.use(express.json());
-// Basically telling express to turn
+// Basically telling express to use a middleware that parses all incoming requests to JSON
 
 app.get("/", function (_, res) {
   res.send("Hello! Yes, we're doing well!");
@@ -20,19 +20,20 @@ app.get("/", function (_, res) {
 app.get("/read-body", function (req, res) {
   // Example of how to read the body of a request.
   // http://localhost:8000/read-body with a body of:
-  // { "good": "thing" } returns the same thing.
+  // { "val": "hi" } returns 'hi'.
   const { body } = req;
   console.log("body:", body);
-  res.send(body);
+  res.send(body.val);
 });
 
 app.get("/read-params/:val", (req, res) => {
   // Example of how to read param.
   // http://localhost:8000/read-params/hello
-  // Would return { "val": "hello" }
+  // with this body: { "val": "hello" }
+  // Would return "hello"
   const { params } = req;
-  console.log("yep", params);
-  res.send(params);
+  console.log("params:", params);
+  res.send(params.val);
 });
 
 app.listen(portNumber, () => console.log(`listening on port ${portNumber}`));
